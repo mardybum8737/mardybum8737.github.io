cabaca14f90c6e2ff5b131de9a2ec859a0d09f35
mardybum8737 <36396926+mardybum8737@users.noreply.github.com>
2023-10-17 21:30:11 +0900

    JS-active_menu2

--- src/active_menu.js
+++ src/active_menu.js
@@ -17,15 +17,32 @@ const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
+const visibleSections = sectionIds.map(() => false);
 
 const options = {};
 const observer = new IntersectionObserver(observercallback, options);
 sections.forEach((section) => observer.observe(section));
 
 function observercallback(entries) {
+  let selectLastOne;
   entries.forEach((entry) => {
-    console.log(entry.target);
-    console.log(entry.isIntersecting);
-    console.log(entry.intersectionRatio);
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    selectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.99;
   });
+  console.log(visibleSections);
+  console.log('무조건 라스트 섹션!!', selectLastOne);
+
+  const navIndex = selectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+  console.log(sectionIds[navIndex]);
+}
+
+function findFirstIntersecting(intersections) {
+  const index = intersections.indexOf(true);
+  return index >= 0 ? index : 0;
 }
